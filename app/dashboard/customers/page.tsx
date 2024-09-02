4b61f3e1bdc00c44d9b484be8b803da5cc95471b
barakatokikiola <barakattajudeen2018@gmail.com>
2024-09-02 12:16:21 +0100

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Invoices(){
+
+    return(
+        <h1>Customers Dashboard Page</h1>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Invoices(){
+
+    return(
+        <h1>Invoice Dashboard Page</h1>
+    )
+}
