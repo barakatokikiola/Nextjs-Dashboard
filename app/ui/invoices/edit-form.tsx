9b59bde4a0508fcadc20e5c9f4ba29a4244ad868
barakatokikiola <barakattajudeen2018@gmail.com>
2024-10-23 22:51:35 +0100

    edit form updated

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -21,7 +21,7 @@ export default function EditInvoiceForm({
 }) {
   const initialState: State = {message: null, errors:{}};
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
-  const [, formAction] = useActionState(updateInvoiceWithId, initialState)
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState)
   return (
     <form action={formAction}>
       
@@ -50,6 +50,15 @@ export default function EditInvoiceForm({
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+
         </div>
 
         {/* Invoice Amount */}
